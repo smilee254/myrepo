8939e87073714f7598839f36c19e5feb47f20a6e
smilee <mwanglewis6@gmail.com>
2026-03-11 21:39:32 +0300

    debug report

--- activate_idcs.sh
+++ activate_idcs.sh
@@ -3,6 +3,6 @@
 # Run this using: source ./activate_idcs.sh
 
 echo "🚀 Activating IDCS Environment from Home Directory..."
-source ~/idcs_venv/bin/activate
+source "/media/smilee/64 GB/new/myrepo/venv/bin/activate"
 echo "✅ Environment Ready!"
 python3 check_env.py
